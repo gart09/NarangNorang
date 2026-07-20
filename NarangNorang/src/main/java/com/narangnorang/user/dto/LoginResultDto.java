5eec27f64416bfdc37a3a6598fefd29b1a587328
seyeonham <seyeon021023@gmail.com>
2026-07-20 19:08:04 +0900

    feat: 로그인 요청 및 응답 구현 DTO

--- NarangNorang/src/main/java/com/narangnorang/user/dto/LoginResultDto.java
+++ NarangNorang/src/main/java/com/narangnorang/user/dto/LoginResultDto.java
@@ -1,4 +1,13 @@
 package com.narangnorang.user.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class LoginResultDto {
+
+    private String result;
+    private String token;
+    private String refreshToken;
 }
