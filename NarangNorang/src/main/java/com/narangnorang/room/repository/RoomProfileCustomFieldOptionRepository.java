d9784fb4024a086538d7a16c70f2ba50442c0b66
gart09 <gart09@naver.com>
2026-07-23 19:13:38 +0900

    feat: 커스텀필드 추가/수정 시 오류 3가지 사항 추가

--- NarangNorang/src/main/java/com/narangnorang/room/repository/RoomProfileCustomFieldOptionRepository.java
+++ NarangNorang/src/main/java/com/narangnorang/room/repository/RoomProfileCustomFieldOptionRepository.java
@@ -2,6 +2,7 @@ package com.narangnorang.room.repository;
 
 import com.narangnorang.room.entity.RoomProfileCustomFieldOption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
