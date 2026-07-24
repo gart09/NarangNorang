d29c20a4fd5be0544d1d865a4adcf1a19289594a
gart09 <69684927+gart09@users.noreply.github.com>
2026-07-24 17:10:39 +0900

    Issue#33/feat chat,member profile card j unit (#47)
    * chore: Jacoco 설정
    
    * chore: settings.gradle내용 삭제
    
    * chore: 자잘한거 삭제

--- NarangNorang/src/test/java/com/narangnorang/NarangNorangApplicationTests.java
+++ NarangNorang/src/test/java/com/narangnorang/NarangNorangApplicationTests.java
@@ -2,8 +2,10 @@ package com.narangnorang;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // H2 설정이 있는 application-test.properties 적용
 class NarangNorangApplicationTests {
 
 	@Test
